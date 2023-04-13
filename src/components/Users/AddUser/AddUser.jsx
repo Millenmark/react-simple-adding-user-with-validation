f63f1c2c2103b66fb76b08de7c6e4d564c645e09
Millen Mark Aquino <millenmark.iv@gmail.com>
2023-04-13 14:30:03 +0800

    fixes and updates
    I fix the css module of error modal component and updates the adduser component

--- src/components/Users/AddUser/AddUser.jsx
+++ src/components/Users/AddUser/AddUser.jsx
@@ -5,6 +5,7 @@ import { Card, Button, ErrorModal } from '../../UI'
 const AddUser = ({ getUsers }) => {
   const [enteredUsername, setEnteredUsername] = useState('')
   const [enteredAge, setEnteredAge] = useState('')
+  const [error, setError] = useState()
 
   const addUserHandler = (e) => {
     e.preventDefault()
@@ -24,17 +25,19 @@ const AddUser = ({ getUsers }) => {
 
 
   return (
-    <Card className={input}>
-      <form onSubmit={addUserHandler}>
-        <label htmlFor="username">Username</label>
-        <input type="text" name="username" id="username" onChange={usernameChangeHandler} value={enteredUsername}/>
-        <label htmlFor="age">Age (Years)</label>
-        <input type="number" name="age" id="age" onChange={ageChangeHandler} value={enteredAge}/>
-        <Button type='submit'>Add User</Button>
-      </form>
-
+    <div>
       <ErrorModal title={'Hello'} message={'This is an error message'}/>
-    </Card>
+
+      <Card className={input}>
+        <form onSubmit={addUserHandler}>
+          <label htmlFor="username">Username</label>
+          <input type="text" name="username" id="username" onChange={usernameChangeHandler} value={enteredUsername}/>
+          <label htmlFor="age">Age (Years)</label>
+          <input type="number" name="age" id="age" onChange={ageChangeHandler} value={enteredAge}/>
+          <Button type='submit'>Add User</Button>
+        </form>
+      </Card>
+    </div>
   )
 }
 
