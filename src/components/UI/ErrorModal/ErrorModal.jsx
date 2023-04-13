49c0d2d5b632a8c77057c676dea2d12c819be0b0
Millen Mark Aquino <millenmark.iv@gmail.com>
2023-04-13 14:18:54 +0800

    adding the backdrop for error modal

--- src/components/UI/ErrorModal/ErrorModal.jsx
+++ src/components/UI/ErrorModal/ErrorModal.jsx
@@ -1,21 +1,24 @@
 import React from 'react'
 import Button from '../Button/Button'
 import Card from '../Card/Card'
-import { modal, header, content, actions } from './ErrorModal.module.css'
+import { backdrop, modal, header, content, actions } from './ErrorModal.module.css'
 
 const ErrorModal = ({title, message}) => {
   return (
-    <Card className={modal}>
-      <header className={header}>
-        <h2>{title}</h2>
-      </header>
-      <div className={content}>
-        <p>{message}</p>
-      </div>
-      <footer className={actions}>
-        <Button>Okay</Button>
-      </footer>
-    </Card>
+    <div>
+      <div className={backdrop}></div>
+      <Card className={modal}>
+        <header className={header}>
+          <h2>{title}</h2>
+        </header>
+        <div className={content}>
+          <p>{message}</p>
+        </div>
+        <footer className={actions}>
+          <Button>Okay</Button>
+        </footer>
+      </Card>
+    </div>
   )
 }
 
